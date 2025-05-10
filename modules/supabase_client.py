0d371b7a7557e9971df83c971c8c3903b278a6aa
halpe-hal <nishimura@kklia.com>
2025-05-10 13:55:36 +0900

    Update supabase_client.py

--- modules/supabase_client.py
+++ modules/supabase_client.py
@@ -1,6 +1,7 @@
 # modules/supabase_client.py
 
 from supabase import create_client
+import streamlit as st
 
 SUPABASE_URL = st.secrets["SUPABASE_URL"]
 SUPABASE_API_KEY = st.secrets["SUPABASE_API_KEY"]
